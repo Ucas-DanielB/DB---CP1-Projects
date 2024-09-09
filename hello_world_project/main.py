a47fe7fba63b7e8d551848e81328bfc5bfa47d3c
Ucas-DanielB <daniel.blanco@ucas-edu.net>
2024-09-09 10:17:18 -0600

    tried to polish whoAreyou assignment

---
+++ hello_world_project/main.py
@@ -0,0 +1,5 @@
+#Daniel Blanco, Hello World
+
+name = input("What is your name?: ")
+
+print("Hello", name)
--- whoAreYou/main.py
+++ whoAreYou/main.py
@@ -1,5 +1,4 @@
 #Daniel blanco, SkillPractice: Who Are You?
-#The users name, users age, the users favorite color, and outputs all those things in one print() in a complete sentence
 
 name = input("What is your name?: ")
 
@@ -7,5 +6,5 @@ age = input("What is your age?: ")
 
 color = input("What is your favorite color?: ")
 
-print("Hello", name, "What how old are you?", age, "wow being", age, "is so cool!", "What is your favorite color?", "What is your favorite color?", color, "I think", color, "is cool too!")
+print("Hello", name, "how old are you?", age, "? wow being", age, "is so cool!", "What is your favorite color?", color, "? I think", color, "is cool too!")
 
