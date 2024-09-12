a14fd1795972333ef1f3b2d1a138cf54b8fd3b47
Ucas-DanielB <daniel.blanco@ucas-edu.net>
2024-09-12 10:41:02 -0600

    finished Setting, Ressetting, and Ressetting assignment

--- Setting, resetting, and resetting assignment/main.py
+++ Setting, resetting, and resetting assignment/main.py
@@ -1,19 +1,11 @@
 #Daniel Blanco, SkillPractice: Setting, Resetting, and Resetting assignment
-#Faculty and Staff attending: 32, Students being awarded: 100, each student is allowed 2 guests, each table seats 12 people.
-
-#1 student cannot attend, adjust to not count them or their guests. **/
-
-#3 teachers cannot attended *****
-
-#15 students only have 1 guest coming
-
-#1 members of the school board are going to attend *****
-
-staff = 29
-students = 99
+staff = 32
+staff = staff -3
+students = 100
 students = students -1
-guests=students*2
+guests = students*2
 seats = 12
 schoolboardmember = 1
+guests = guests-15
 
 print((staff+students+guests+schoolboardmember)/seats)
