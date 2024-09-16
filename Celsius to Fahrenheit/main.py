0b55eba861349f7fb0757a6c598881cc8ebf269e
Ucas-DanielB <daniel.blanco@ucas-edu.net>
2024-09-16 10:42:53 -0600

    celsius to fahrenheit assignment

---
+++ Celsius to Fahrenheit/main.py
@@ -0,0 +1,9 @@
+#Daniel Blanco, Celsius to Fahrenheit assignment
+
+#(Celsius * 9/5)+32
+
+celsius = int(input("Enter temperature in Celsius: "))
+
+fahrenheit = (celsius * 9/5)+32
+
+print("when it is", celsius, "it is", fahrenheit, "degrees fahrenheit")
