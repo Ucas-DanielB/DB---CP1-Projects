166d2bd2a45cde8dc85f5479aaf8350cc354364d
Ucas-DanielB <daniel.blanco@ucas-edu.net>
2024-09-25 10:50:59 -0600

    made Madlib

---
+++ Madlib/main.py
@@ -0,0 +1,14 @@
+#Your madlib needs to be 4-5 sentences
+#Your madlib needs to have at least 5 words that are user inputs (Stored in variables)
+#Your madlib needs to use 2 of the user inputs twice (for you have a variable called place that you use in 2 different spots in the madlib)
+
+name = input("what is your name? ")
+familyMember = input("Who is your favorite family member?")
+store = input("You feel hungry, where do you go?(a gas station like 7'11) ")
+food = input("What food do you buy? ")
+place = input("Where do you go to eat? ")
+
+print(name, "is done with work for today, and decides to go to his favorite place to eat in the world!", store, "! they go to", place, 
+      "to eat their definitely nutricious", food, "and regain their energy to work another 10 hours straight!" " but whats this?? your", familyMember, "demands money and your lunch to provide for their own family that you have never even met!"
+      "Luckily, they don't seem to realize who they are talking to, so you obliterate them with an earth shattering hook and smash their skull and flee the scene!"
+      "the end :)")
