2ab00c6e64327d6e8f6a8044d70e9a1fd625b585
Ucas-DanielB <daniel.blanco@ucas-edu.net>
2024-09-27 10:20:50 -0600

    completed Simple Calculator

---
+++ Simple Calculator/main.py
@@ -0,0 +1,32 @@
+
+
+#Daniel Blanco, ProfiencyTest: Simple Calculator
+
+int1 = input("Enter your first arithmetic number; ")
+int1 = int(int1)
+int2 = input("Enter your second arithmetic number; ")
+int2 = int(int2)
+
+print(int1 ** int2)
+
+print(int1 // int2)
+
+print(int1 % int2)
+
+print(int1 + int2)
+
+print(int1 - int2)
+
+print(int1 * int2)
+
+print(int1 / int2)
+
+print(int1 == int2)
+
+print(int1 >= int2)
+
+print(int1 <= int2)
+
+print(abs(int1))
+
+print(abs(int2))
