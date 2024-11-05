7b6f9dd6c84edbcaa74df5f2e694f09fbeb82a9f
Ucas-DanielB <daniel.blanco@ucas-edu.net>
2024-11-05 10:44:07 -0700

    Authorized Conditionals Test

---
+++ Authorized/main.py
@@ -0,0 +1,86 @@
+#Write a program that checks if a user is in the list of authorized users 
+#(HINT: you can use the keyword 'in' to check if a value is in a list). 
+# And then print out if they are an admin, uesr, or not authorized. 
+# (Yes that means you need to set at least 1 admin) 
+
+#Authorized users should have at least 8 people on it
+
+#Type if variable is >= int: print("")
+
+a = ("You are authorized ")
+n = ("You are not authorized ")
+d = ("WOW! an admin! ")
+
+
+user1 = input("Does the super secret password to be an authorized user the number 1 or 10?(1) ")
+user2 = input("Which one? 1 or 10?(1) ")
+user3 = input("Well? 1 or 10?(1) ")
+user4 = input("So is it 1 or 10?(1) ")
+user5 = input("Hey! don't lose focus! is it 1 or 10?(1) ")
+user6 = input("Which is it?? 1 or 10?(1) ")
+user7 = input("Umm, 1 or 10 man?(1) ")
+user8 = input("Only admins can see this... but the answer for admin is 5: ")
+user9 = input("You still have to answer if it is 1 or 10(1) ")
+user10 = input("Come on answer! 1 or 10?!(10) ")
+
+
+
+
+if user1 is 1:
+    print(n)
+else:
+    print(a)
+
+
+if user2 is 1:
+    print(n)
+else:
+    print(a)
+
+
+if user3 is 1:
+    print(n)
+else:
+    print(a)
+
+
+if user4 is 1:
+    print(n)
+else:
+    print(a)
+
+
+if user5 is 1:
+    print(n)
+else:
+    print(a)
+
+
+if user6 is 1:
+    print(n)
+else:
+    print(a)
+
+
+if user7 is 1:
+    print(n)
+else:
+    print(a)
+
+
+if user8 is 5:
+    print(n)
+else:
+    print(d)
+
+
+if user9 is 1:
+    print(n)
+else:
+    print(a)
+
+
+if user10 is 10:
+    print(a)
+else:
+    print(n)
