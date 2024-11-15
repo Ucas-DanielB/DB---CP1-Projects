b490453c9616012641f8f9e8b8ea5aa2e63c2394
Ucas-DanielB <daniel.blanco@ucas-edu.net>
2024-11-15 10:50:10 -0700

    What are these numbers?

---
+++ What are these numbers/main.py
@@ -0,0 +1,20 @@
+#Write a program that takes in a number from the user and then format that number as:
+
+#an integer with the comma to separate the thousands
+#a float with 4 decimal places
+#a percentage
+#and then a dollar amount
+
+#you can have more than one user input and ask for different numbers
+
+#The output needs to properly incorporate the number into a sentence (for each of the outputs) 
+
+#num,000 = input("Give me a number: ")
+#num4. = input("Give me a number: ")
+#num% = input("Give me another: ")
+#num$ = input("Don't be shy give me another: ")
+
+#print("")
+#print("")
+#print("")
+#print("")
